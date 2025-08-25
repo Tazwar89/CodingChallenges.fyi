b9ad12a4623e6eebe896d3823496e6ee066d7f4a
Tazwar Sikder <tazwar_rocks@hotmail.com>
2025-08-25 05:04:05 -0400

    Passed Step Two
    Matched output for Challenge 1: Step Two
    Also fixed conditional statement checking arg[0] for command line options -c or -l

--- src/model/ccwc.java
+++ src/model/ccwc.java
@@ -1,22 +1,53 @@
 package model;
 
+import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class ccwc {
 	public static void main(String[] args) {
 		try {
-			if (!args[0].equals("-c")) {
-				throw new InvalidArgumentException("Invalid command line argument! Must be '-c'.");
+			if (!args[0].equals("-c") && !args[0].equals("-l")) {
+				throw new InvalidArgumentException("Invalid command line argument! Must be '-c' or '-l'.");
 			}
 
-			String filename = args[1];
-			File file = new File(filename);
+			File file = new File(args[1]);
+			Scanner reader = new Scanner(file);
 
-			System.out.printf("%d %s", file.length(), filename);
+			switch (args[0]) {
+				case "-c": {
+					System.out.printf("%d %s", file.length(), args[1]);
+
+					break;
+				}
+
+				case "-l": {
+					int lineCount = 0;
+
+					while (reader.hasNextLine()) {
+						reader.nextLine();
+						lineCount++;
+					}
+
+					reader.close();
+
+					System.out.printf("%d %s", lineCount, args[1]);
+
+					break;
+				}
+
+				default: {
+					break;
+				}
+			}
 		}
 
 		catch (InvalidArgumentException iae) {
 			System.out.println(iae.getMessage());
+		}
+
+		catch (FileNotFoundException fnfe) {
+			System.out.println("File not found!");
 		}
 	}
 }
