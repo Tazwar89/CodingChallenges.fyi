9cb41a460a3bdb389342fd2bc50a9321d422e73c
Tazwar Sikder <tazwar_rocks@hotmail.com>
2025-08-25 05:45:19 -0400

    Passed Step Three / Step Four
    Matched outputs for steps three and four.
    
    Correctly counted all characters, after initially stripping all whitespace (but this was wrong). Also, Scanner methods did not account for locale differences, so BufferedReader methods were used instead).
    
    Debugged double counting lines in file, which led to NoSuchElementException (no line found).

--- src/model/ccwc.java
+++ src/model/ccwc.java
@@ -1,14 +1,17 @@
 package model;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ccwc {
 	public static void main(String[] args) {
 		try {
-			if (!args[0].equals("-c") && !args[0].equals("-l")) {
-				throw new InvalidArgumentException("Invalid command line argument! Must be '-c' or '-l'.");
+			if (!args[0].equals("-c") && !args[0].equals("-l") && !args[0].equals("-w") && !args[0].equals("-m")) {
+				throw new InvalidArgumentException("Invalid command line argument! Must be '-c', '-l', '-w', or '-m.");
 			}
 
 			File file = new File(args[1]);
@@ -36,6 +39,36 @@ public class ccwc {
 					break;
 				}
 
+				case "-w": {
+					int wordCount = 0;
+
+					while (reader.hasNext()) {
+						reader.next();
+						wordCount++;
+					}
+
+					reader.close();
+
+					System.out.printf("%d %s", wordCount, args[1]);
+
+					break;
+				}
+
+				case "-m": {
+					BufferedReader read = new BufferedReader(new FileReader(args[1]));
+					int charCount = 0;
+
+					while (read.read() != -1) {
+						charCount++;
+					}
+
+					read.close();
+
+					System.out.printf("%d %s", charCount, args[1]);
+
+					break;
+				}
+
 				default: {
 					break;
 				}
@@ -48,6 +81,10 @@ public class ccwc {
 
 		catch (FileNotFoundException fnfe) {
 			System.out.println("File not found!");
+		}
+
+		catch (IOException ioe) {
+			System.out.println("File could not be read!");
 		}
 	}
 }
